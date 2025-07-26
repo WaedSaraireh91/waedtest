4df02be29d9b6193f8e739a248d5796ffe333e10
Waed <wsarayreh@ainnova.com>
2025-07-26 15:54:17 +0300

    Initial Commit

---
+++ native iOS Application - Waed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  native iOS Application - Waed
+//
+//  Created by Majd Attely on 26/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
